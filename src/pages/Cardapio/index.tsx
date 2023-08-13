9e2c71f9e4167b83ad065c0aaa38e7a62cb096f4
KatiaSonally <katiasonally@yahoo.com.br>
2023-08-13 20:30:55 -0300

    feat: implements the orderer

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,6 +3,7 @@ import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
 import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
@@ -18,8 +19,10 @@ export default function Cardapio() {
             <section className={styles.cardapio}>
                 <h3 className={styles.cardapio__titulo}>Cardápio</h3>
                 <Buscador busca={busca} setBusca={setBusca} /> 
-                <div className={styles.cardapio__filtros}></div>
-                <Filtros filtro={filtro} setFiltro={setFiltro} />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro} />
+                    <Ordenador />
+                </div>
             </section>
         </main>
     )
